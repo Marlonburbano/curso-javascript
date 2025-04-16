36b5f9f9a0ea0a4f1ee0ae3a438c4e8449d95fd4
Marlon Burbano Cabrera <marlonburbano2899@gmail.com>
2025-04-15 22:38:59 -0500

    operadores

---
+++ 01-tipos/09-argumentos.js
@@ -0,0 +1,12 @@
+function suma(num1, num2) { // <=== se llaman parametros 
+    return num1 + num2
+
+}
+
+let resultado = suma(3, 4)// <=== se llaman argumentos
+console.log(resultado)
+
+// se puede recibir multiples argumentos y multiples 
+//parametros
+
+console.log(typeof suma)
---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,19 @@
+// para realizar operaciones matematicas 
+
+let a = 5
+let b = 3
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+// console.log(a ** b)
+
+//incrementar 
+console.log(++a)
+console.log(++a)
+// decrementar
+
+console.log(--a)
+console.log(--a)
