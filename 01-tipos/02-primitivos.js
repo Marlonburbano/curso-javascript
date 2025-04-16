a5d84f01eb1891f9ca375a6b75247cf3420cf0f3
Marlon Burbano Cabrera <marlonburbano2899@gmail.com>
2025-04-15 21:31:07 -0500

    agregando mas cosas

--- 01-tipos/02-primitivos.js
+++ 01-tipos/02-primitivos.js
@@ -1,7 +1,11 @@
-let numero = 1; // number
-let texto = "hola mundo"; // string
-let verdadero = true; // bolean
-let falso = false;// bolean
-let noDefinido ; 
+let numero = 1; 
+let texto  = 'Esto es un string';
+let verdadero = true; 
+let falso = false;
+let noDefinido;
 let undef = undefined;
-let nulo = null;
+let nulo = null; 
+
+
+//null se utiliza para  decir que
+//  tiene un valor pero no esta definido 
--- 01-tipos/03-constantes.js
+++ 01-tipos/03-constantes.js
@@ -2,9 +2,11 @@ let nombre = 'hola mundo'
 nombre = "marlon burbano" ////valor de la variable ultimo que se le da
 console.log(nombre)
 
-
 //////////////////
 
 const name = 'MARLON'
-//SOLO TOMA UN VALOR Y NO SE LO PUEDE CAMBIAR COMO ARRIBA 
+console.log(name)
+//SOLO TOMA UN VALOR Y 
+// NO SE LO PUEDE CAMBIAR COMO ARRIBA 
+//A ESTO SE LE CONOCE COMO TIPADO ESTATICO
 
